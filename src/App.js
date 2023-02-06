bb086c5fc8fe86ac41847b12ba115cc96d960659
phamduy0165 <75572336+phamduy0165@users.noreply.github.com>
2023-02-06 23:33:06 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Đây là Content</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
